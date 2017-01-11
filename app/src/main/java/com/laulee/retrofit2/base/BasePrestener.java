30d9ee5e9d98637608d47bd5fbc2c88a3a1b904c
Laulee <756162314@qq.com>
2017-01-11 18:29:55 +0800

    change BasePresenter interface name

--- app/src/main/java/com/laulee/retrofit2/base/BasePrestener.java
+++ app/src/main/java/com/laulee/retrofit2/base/BasePrestener.java
@@ -4,7 +4,7 @@ package com.laulee.retrofit2.base;
  * Created by laulee on 16/12/25.
  */
 
-public class BasePrestener<T extends BaseView> implements BasePrestenerInterface<T> {
+public class BasePrestener<T extends BaseView> implements IBasePrestener<T> {
 
     protected T mView;
 
--- app/src/main/java/com/laulee/retrofit2/base/IBasePrestener.java
+++ app/src/main/java/com/laulee/retrofit2/base/IBasePrestener.java
@@ -4,7 +4,7 @@ package com.laulee.retrofit2.base;
  * Created by laulee on 16/12/25.
  */
 
-public interface BasePrestenerInterface<V extends BaseView> {
+public interface IBasePrestener<V extends BaseView> {
 
     void attach(V view);
 
